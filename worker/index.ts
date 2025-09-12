fe38e6b76a6138456c6f8bd778dbebddb6099980
Nicholas Desai <nicholassdesai@gmail.com>
2025-09-12 14:39:20 -0500

    patch: remove worker stuff

--- worker/index.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    /* eslint-disable */
-    fetch: async (request, env) => {
-        void request;
-        void env;
-        return new Response(null, { status: 404 });
-    },
-} satisfies ExportedHandler<Env>;
